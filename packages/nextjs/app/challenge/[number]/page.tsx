4a00a7ed902687334138db270fd06d1817fcc8dd
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-09-19 22:46:05 +0800

    style: Challenge Page (#27)

--- packages/nextjs/app/challenge/[number]/page.tsx
+++ packages/nextjs/app/challenge/[number]/page.tsx
@@ -1,4 +1,5 @@
 import { notFound } from "next/navigation";
+import clsx from "clsx";
 import fs from "fs";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import path from "path";
@@ -10,6 +11,8 @@ interface ChallengePageProps {
     number: string;
   };
 }
+
+const borderStyles = "absolute border-green-400 h-3 w-3";
 
 export async function generateMetadata({ params }: ChallengePageProps) {
   return getMetadata({
@@ -30,15 +33,30 @@ export default async function ChallengePage({ params }: ChallengePageProps) {
   }
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="mb-4 text-3xl md:text-4xl font-bold font-dotGothic tracking-wide">Challenge {number}</h1>
-      <div className="prose prose-invert">
-        <div className="mb-6">
-          {/* TODO: fetch the contract address (some challenges might not have one (e.g. offchain backend challenges)) */}
-          <Address address="0x0000000000000000000000000000000000000000" />
+    <div className="py-20 px-6 min-h-screen bg-[url(/dot-texture.svg)]">
+      <div className="max-w-3xl mx-auto bg-base-100 border-2 border-t-4 border-l-4 border-green-700 border-t-green-600 border-l-green-500">
+        <div className="px-6 py-2 bg-green-600/30 border-b border-green-600">
+          <h1 className="mt-[3px] text-xl leading-none font-bold font-dotGothic tracking-wide">
+            &gt; Challenge #{number}
+            <span className="ml-1 animate-pulse">_</span>
+          </h1>
         </div>
-        {/* Format the Markdown (bold, titles, etc) */}
-        <MDXRemote source={content} />
+        <div className="px-6 py-8">
+          <div className="mb-8 p-2 relative w-72 flex justify-center items-center font-dotGothic">
+            <div className={clsx(borderStyles, "top-0 left-0 border-t border-l")}></div>
+            <div className={clsx(borderStyles, "top-0 right-0 border-t border-r")}></div>
+            <div className={clsx(borderStyles, "bottom-0 left-0 border-b border-l")}></div>
+            <div className={clsx(borderStyles, "bottom-0 right-0 border-b border-r")}></div>
+            {/* TODO: fetch the contract address (some challenges might not have one (e.g. offchain backend challenges)) */}
+            <span className="mr-3 hidden md:inline-block">Contract:</span>{" "}
+            <Address address="0x0000000000000000000000000000000000000000" />
+          </div>
+
+          {/* Format the Markdown (bold, titles, etc) */}
+          <div className="prose prose-headings:font-dotGothic prose-headings:tracking-wide prose-h1:text-3xl">
+            <MDXRemote source={content} />
+          </div>
+        </div>
       </div>
     </div>
   );
