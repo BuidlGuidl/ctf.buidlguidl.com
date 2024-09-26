4e96e7b78bb2543698a0e0de535789fbb98ce188
Damian Martinelli <damianmarti@gmail.com>
2024-09-26 10:29:38 -0300

    Challenge 7 (#37)

--- packages/hardhat/contracts/NFTFlags.sol
+++ packages/hardhat/contracts/NFTFlags.sol
@@ -2,13 +2,14 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 
 // TODO: Think if ERC1155 is a better choice
 // TODO: Check if the user have registered to the ctf before minting (Challenge1)?
-contract NFTFlags is ERC721, Ownable {
+contract NFTFlags is ERC721, IERC721Receiver , Ownable {
     using Strings for uint256;
 
     mapping(address => bool) public allowedMinters;
@@ -24,6 +25,10 @@ contract NFTFlags is ERC721, Ownable {
 
     function mint(address _recipient, uint256 _challengeId) external {
         require(allowedMinters[msg.sender], "Not allowed to mint");
+        _mintToken(_recipient, _challengeId);
+    }
+
+    function _mintToken(address _recipient, uint256 _challengeId) internal {
         require(!hasMinted[_recipient][_challengeId], "Address has already minted for this challenge");
 
         tokenIdCounter++;
@@ -84,5 +89,45 @@ contract NFTFlags is ERC721, Ownable {
 
     function removeAllowedMinter(address minter) external onlyOwner {
         allowedMinters[minter] = false;
+    }
+
+    // https://github.com/GNSPS/solidity-bytes-utils/blob/master/contracts/BytesLib.sol#L374
+    function _toUint256(bytes memory _bytes) internal pure returns (uint256) {
+        require(_bytes.length >= 32, "toUint256_outOfBounds");
+        uint256 tempUint;
+
+        assembly {
+            tempUint := mload(add(_bytes, 0x20))
+        }
+
+        return tempUint;
+    }
+
+    function onERC721Received(
+        address,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external override returns (bytes4) {
+        uint256 anotherTokenId = _toUint256(data);
+
+        require(
+            msg.sender == address(this),
+            "only this contract can call this function!"
+        );
+
+        require(
+            ownerOf(anotherTokenId) == from, 
+            "Not owner!"
+        );
+
+        require(tokenIdToChallengeId[tokenId] == 1, "Not the right token 1!");
+        require(tokenIdToChallengeId[anotherTokenId] == 6, "Not the right token 6!");
+
+        _mintToken(from, 7);
+
+        safeTransferFrom(address(this), from, tokenId);
+
+        return this.onERC721Received.selector;
     }
 }
