668d9566aae76d0e188cae30cfcd084e035eea28
Carlos Sánchez <oceanrdn@gmail.com>
2024-10-23 11:14:21 +0200

    Challenge 14: Unverified contract (#69)

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -140,6 +140,31 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
 
   console.log("🚩 Challenge #13 deployed");
 
+  const challenge14BytecodeBase =
+    "0x608060405234801561001057600080fd5b5060405161022c38038061022c83398101604081905261002f91610054565b600080546001600160a01b0319166001600160a01b0392909216919091179055610084565b60006020828403121561006657600080fd5b81516001600160a01b038116811461007d57600080fd5b9392505050565b610199806100936000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c80638fd628f01461003b578063d56d229d14610050575b600080fd5b61004e610049366004610133565b61007f565b005b600054610063906001600160a01b031681565b6040516001600160a01b03909116815260200160405180910390f35b6001600160a01b03811633146100cc5760405162461bcd60e51b815260206004820152600e60248201526d24b73b30b634b21036b4b73a32b960911b604482015260640160405180910390fd5b6000546040516340c10f1960e01b8152336004820152600e60248201526001600160a01b03909116906340c10f1990604401600060405180830381600087803b15801561011857600080fd5b505af115801561012c573d6000803e3d6000fd5b5050505050565b60006020828403121561014557600080fd5b81356001600160a01b038116811461015c57600080fd5b939250505056fea264697066735822122012c954f090d228ec3f9a3f4c43c3aad1ad6038576f27eb30eb52706c543a585564736f6c63430008140033";
+  const nftFlagsAddress = await nftFlags.getAddress();
+  const challenge14Bytecode = challenge14BytecodeBase + nftFlagsAddress.slice(2).padStart(64, "0");
+  const deployerSigner = await hre.ethers.getSigner(deployer);
+  const nonce = await deployerSigner.getNonce();
+
+  const feeData = await hre.ethers.provider.getFeeData();
+  const rawTx = {
+    nonce: nonce,
+    maxFeePerGas: feeData.maxFeePerGas,
+    maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
+    gasLimit: 200_000,
+    to: null,
+    value: 0,
+    data: challenge14Bytecode,
+    chainId: (await hre.ethers.provider.getNetwork()).chainId,
+  };
+
+  const txResponse = await deployerSigner.sendTransaction(rawTx);
+  const txReceipt = await txResponse.wait();
+  const challenge14Address = txReceipt?.contractAddress;
+
+  console.log("🚩 Challenge #14 deployed at:", challenge14Address);
+
   // Set addAllowedMinterMultiple in NFTFlags
   const challengeAddresses = [
     await (await hre.ethers.getContract<Contract>("Challenge1", deployer)).getAddress(),
@@ -153,6 +178,7 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
     await (await hre.ethers.getContract<Contract>("Challenge11", deployer)).getAddress(),
     await challenge12Contract.getAddress(),
     await (await hre.ethers.getContract<Contract>("Challenge13", deployer)).getAddress(),
+    challenge14Address,
   ];
 
   const tx = await nftFlags.addAllowedMinterMultiple(challengeAddresses);
