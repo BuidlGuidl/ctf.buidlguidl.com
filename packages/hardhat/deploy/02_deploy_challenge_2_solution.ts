d1b8126c44cae62525fc6e15610a94bfa91be1c0
Carlos Sánchez <oceanrdn@gmail.com>
2024-11-08 18:13:33 +0100

    deploy naming (#118)

--- packages/hardhat/deploy/02_deploy_challenge_2_solution.ts
+++ packages/hardhat/deploy/02_deploy_challenge_2_solution.ts
@@ -2,11 +2,11 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 
 /**
- * Deploys the Challenge2Solution contract
+ * Deploys a challenge solution contract
  *
  * @param hre HardhatRuntimeEnvironment object.
  */
-const deployChallenge2Solution: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployChallengeSolution: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   // This is the deployer account:
   // - localhost: hardhat account 0
   // - live network: PK in .env file (use `yarn generate` to generate one or fill the .env file with your own PK)
@@ -20,11 +20,11 @@ const deployChallenge2Solution: DeployFunction = async function (hre: HardhatRun
   //     autoMine: true,
   //   });
   //
-  //   console.log("🚩 Challenge2Solution contract deployed");
+  //   console.log("🚩 Challenge Solution contract deployed");
 };
 
-export default deployChallenge2Solution;
+export default deployChallengeSolution;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags solution2
-deployChallenge2Solution.tags = ["solution2"];
+deployChallengeSolution.tags = ["solution2"];
